85d8f54eb73dbf0121ca6ac3726a946580cf2ca0
tmccolgan88 <mccolgantim@gmail.com>
2015-08-10 22:35:06 +0000

    updating errthang

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*function isGamerModalOpen() {
+  console.log("in isGamerModal()");
+  $("#popup1").dialog({
+    height: 35,
+    width: 75,
+    dialogClass: 'no-close popup1'
+  }).dialog("widget").find(".ui-dialog-title").hide();
+}
+
+function isGamerModalClose(){
+  console.log("in leaving isGamerModal()");
+  $("#popup1").dialog("close");
+}
+
+*/
